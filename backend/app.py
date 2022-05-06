b9b71ec449e1937567bc53f134053dc10380c944
mrdanf <dancoding@gmx.net>
2022-05-06 14:47:28 +0200

    Linked Front- and Backend. Printing 'Hello World' to console

--- backend/app.py
+++ backend/app.py
@@ -5,9 +5,16 @@ from flask_marshmallow import Marshmallow
 from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:abc@localhost:13306/portal'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():  # put application's code here
     return 'Hello World!'
 
