cc66a26725780c5b6a8242a59c987c6786efff56
mrdanf <dancoding@gmx.net>
2022-05-23 20:35:40 +0200

    Added .env file for reading database URI, renamed app.py to flask_backend.py, added db_init.py for easy create_drop of database tables

---
+++ backend/db_init.py
@@ -0,0 +1,6 @@
+from flask_backend import app, db
+
+with app.test_request_context():
+    db.init_app(app)
+    db.drop_all()
+    db.create_all()
--- backend/flask_backend.py
+++ backend/flask_backend.py
@@ -4,12 +4,13 @@ import datetime
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
 import pika
+from dotenv import dotenv_values
 
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:abc@localhost:13306/portal'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+config = dotenv_values(".env")
+app.config['SQLALCHEMY_DATABASE_URI'] = config["DB_FULL_URI"]
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -20,18 +21,20 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     forename = db.Column(db.String(50))
     lastname = db.Column(db.String(50))
+    gender = db.Column(db.String(50))
     password = db.Column(db.String(50))
     creation_date = db.Column(db.DateTime, default=datetime.datetime.now())
 
-    def __init__(self, forename, lastname, password):
+    def __init__(self, forename, lastname, gender, password):
         self.forename = forename
         self.lastname = lastname
+        self.gender = gender
         self.password = password
 
 
 class UserSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'forename', 'lastname', 'password', 'creation_date')
+        fields = ('id', 'forename', 'lastname', 'gender', 'password', 'creation_date')
 
 
 user_schema = UserSchema()
@@ -47,9 +50,10 @@ def hello_world():
 def create_account():
     forename = request.json['forename']
     lastname = request.json['lastname']
+    gender = request.json['gender']
     password = request.json['password']
 
-    user = User(forename, lastname, password)
+    user = User(forename, lastname, gender, password)
     db.session.add(user)
     db.session.commit()
 
