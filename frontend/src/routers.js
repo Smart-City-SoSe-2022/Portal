d93b7327d6e97a6b3c2567cb36f84ec06668b23b
mrdanf <dancoding@gmx.net>
2022-05-23 20:37:23 +0200

    Added CreateAccount.vue, LoginPage.vue and LandingPage.vue pages, without functionality

--- frontend/src/routers.js
+++ frontend/src/routers.js
@@ -1,17 +1,29 @@
 import {createRouter, createWebHistory} from "vue-router";
-import HelloWorld from "@/components/HelloWorld";
 import MessageSend from "@/components/MessageSend";
+import CreateAccount from "@/components/CreateAccount";
+import LandingPage from "@/components/LandingPage";
+import LoginPage from "@/components/LoginPage";
 
 const routes = [
     {
         path: '/',
-        name: 'helloWorld',
-        component: HelloWorld
+        name: 'landingPage',
+        component: LandingPage
     },
     {
         path: '/message',
         name: 'messageSend',
         component: MessageSend
+    },
+    {
+        path: '/login',
+        name: 'loginPage',
+        component: LoginPage
+    },
+    {
+        path: '/create',
+        name: 'createAccount',
+        component: CreateAccount
     }
 ]
 
