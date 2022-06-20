51c8a2aeb1a7ace6e6a4bb31bcde0015320db92e
mrdanf <dancoding@gmx.net>
2022-06-20 17:18:01 +0200

    removed passwort vergessen

--- frontend/src/components/LoginPage.vue
+++ frontend/src/components/LoginPage.vue
@@ -28,7 +28,6 @@
               <div class="mb-3">
                 <button class="btn btn-primary d-block w-100" type="submit">Login</button>
               </div>
-              <p class="text-muted">Passwort vergessen?</p>
             </form>
           </div>
         </div>
